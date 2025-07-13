8f32cc3078318e35dbcc296af633f1309cc71d10
NagastyCods <cnagasty@gmail.com>
2025-07-13 08:17:39 +0100

    Made server,js to serve static files at index.html

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public',  'Login.html'));
+    res.sendFile(path.join(__dirname, 'public',  'index.html'));
 });
 
 // Start the server
